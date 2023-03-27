627b79b260e5c9923db71bb7931a20f8ef506a1e
Log-in-off <1386348@mail.ru>
2023-03-27 11:23:20 +0400

    add fast way task 64

--- 64_minimum_path_sum/main.cpp
+++ 64_minimum_path_sum/main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 #include <cassert>
 #include <vector>
+#include <algorithm>
 
+/*fast way*/
+#if 1
+class Solution {
+public:
+    int minPathSum(std::vector<std::vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        for (int i = 1; i < m; i++) {
+            grid[i][0] += grid[i-1][0];
+        }
+        
+        for (int j = 1; j < n; j++) {
+            grid[0][j] += grid[0][j-1];
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] += std::min(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+        
+        return grid[m-1][n-1];
+    }
+};
+#endif
+
+/*my slow way*/
+#if 0
 class Solution {
 public:
     struct edge
@@ -9,7 +39,6 @@ public:
         int target;
         int wight;
     };
-
     int minPathSum(std::vector<std::vector<int>>& grid) {
         const int n = grid.size()* grid[0].size();
         std::vector<std::vector<edge>> graph(n+1);
@@ -67,6 +96,7 @@ public:
         return vertexes[n];
     }
 };
+#endif
 
 /*
 Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
